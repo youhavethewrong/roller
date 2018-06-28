b1f063e1fa09468766e39bb0f3d43265d07106f4
ESC <youhavethewrong@gmail.com>
2018-06-27 19:26:50 -0500

    Initial

---
+++ src/roller/core.clj
@@ -0,0 +1,42 @@
+(ns roller.core
+  (:require [clojure.java.io :as io])
+  (:gen-class))
+
+(defn find-all-files
+  [f]
+  (if (and (.isFile f) (not (.isHidden f)))
+    f
+    (flatten (map #(find-all-files %) (.listFiles f)))))
+
+(defn filter-files
+  "Finds file-like structures that are not hidden according to the OS."
+  [dirlist dest]
+  (filter #(and (.contains (.getName %) ".org")
+                (not=(.getName %) dest)
+                (not (.contains (.getAbsolutePath %) ".git"))) dirlist))
+
+(defn sort-files
+  [dirlist]
+  (sort #(compare (.getAbsolutePath %1) (.getAbsolutePath %2)) dirlist))
+
+(defn append-all
+  [dirlist dest]
+  (with-open [w (io/writer dest)]
+    (doseq [f dirlist]
+      (with-open [r (io/reader f)]
+        (.append w (str "* " (.getPath f) "\n"))
+        (doseq [l (line-seq r)]
+          (if (.startsWith l "*")
+            (.append w (str "*" l "\n"))
+            (.append w (str l "\n"))))))))
+
+(defn -main
+  [& args]
+  (if (= 2 (count args))
+    (let [src (io/file (first args))
+          dest (second args)]
+      (-> (find-all-files src)
+          (filter-files dest)
+          (sort-files)
+          (append-all dest)))
+    (println "Usage: java -jar roller.jar source-directory destination-file")))
